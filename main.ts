a107e74275c3e72156ffc060575b045394507c79
BlueOctopusKing <92817520+BlueOctopusKing@users.noreply.github.com>
2021-11-09 13:48:59 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,27 +8,49 @@ let Stored_D2 = 0
 let dice_2 = 0
 let Stored_D4 = 0
 let dice_4 = 0
+let Shake_count = 0
 input.onPinPressed(TouchPin.P0, function () {
-    basic.showIcon(IconNames.Yes)
-    Stored_D3 = dice_3
+    if (Stored_D3 > 0) {
+        Stored_D3 = 0
+    } else {
+        basic.showIcon(IconNames.Yes)
+        Stored_D3 = dice_3
+    }
 })
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Yes)
-    Stored_D1 = dice_1
+    if (Stored_D1 > 0) {
+        Stored_D1 = 0
+    } else {
+        basic.showIcon(IconNames.Yes)
+        Stored_D1 = dice_1
+    }
 })
 input.onPinPressed(TouchPin.P2, function () {
-    basic.showIcon(IconNames.Yes)
-    Stored_D5 = dice_5
+    if (Stored_D5 > 0) {
+        Stored_D5 = 0
+    } else {
+        basic.showIcon(IconNames.Yes)
+        Stored_D5 = dice_5
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Yes)
-    Stored_D2 = dice_2
+    if (Stored_D2 > 0) {
+        Stored_D2 = 0
+    } else {
+        basic.showIcon(IconNames.Yes)
+        Stored_D2 = dice_2
+    }
 })
 input.onPinPressed(TouchPin.P1, function () {
-    basic.showIcon(IconNames.Yes)
-    Stored_D4 = dice_4
+    if (Stored_D4 > 0) {
+        Stored_D4 = 0
+    } else {
+        basic.showIcon(IconNames.Yes)
+        Stored_D4 = dice_4
+    }
 })
 input.onGesture(Gesture.Shake, function () {
+    Shake_count = Shake_count + 1
     dice_1 = randint(1, 6)
     dice_2 = randint(1, 6)
     dice_3 = randint(1, 6)
@@ -36,27 +58,15 @@ input.onGesture(Gesture.Shake, function () {
     dice_5 = randint(1, 6)
 })
 basic.forever(function () {
-    if (true) {
-        basic.showString("D1")
-        basic.showNumber(dice_1)
-        basic.showString("D2")
-        basic.showNumber(dice_2)
-        basic.showString("D3")
-        basic.showNumber(dice_3)
-        basic.showString("D4")
-        basic.showNumber(dice_4)
-        basic.showString("D5")
-        basic.showNumber(dice_5)
+    if (input.buttonIsPressed(Button.AB)) {
+        if (true) {
+            basic.showString("")
+            basic.showNumber(Stored_D1)
+        } else {
+            basic.showString("S#1")
+            basic.showIcon(IconNames.No)
+        }
     } else {
-        basic.showString("D1")
-        basic.showNumber(Stored_D1)
-        basic.showString("D2")
-        basic.showNumber(Stored_D2)
-        basic.showString("D3")
-        basic.showNumber(Stored_D3)
-        basic.showString("D4")
-        basic.showNumber(Stored_D4)
-        basic.showString("D5")
-        basic.showNumber(Stored_D5)
+    	
     }
 })
